b2f4d5272c3164c54cf9515035f8f8f35e0f209b
Navdeep Singh <navdeep.er@gmail.com>
2017-04-12 18:18:54 +0530

    update

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,9 @@
 module.exports = function() {
   return function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
   }
 }
